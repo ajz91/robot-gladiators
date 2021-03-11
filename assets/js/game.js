c9f3485d78356d831554c0e845a46fee98736852
AJ Ziarek <deltrigger@pm.me>
2021-03-11 11:39:25 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,13 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -27,17 +34,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
 
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-    );
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -52,11 +58,9 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
-      enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
-    );
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
 
     // check player's health
     if (playerHealth <= 0) {
@@ -83,7 +87,7 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       fight(pickedEnemyName);
 
@@ -141,6 +145,7 @@ var shop = function() {
 
 // use switch to carry out action
 switch (shopOptionPrompt) {
+  case "REFILL":
   case "refill":
     if (playerMoney >= 7) {
       window.alert("Refilling player's health by 20 for 7 dollars.");
@@ -154,6 +159,7 @@ switch (shopOptionPrompt) {
     }
   
     break;
+  case "UPGRADE":
   case "upgrade":
     if (playerMoney >= 7) {
       window.alert("Upgrading player's attack by 6 for 7 dollars.");
@@ -167,6 +173,7 @@ switch (shopOptionPrompt) {
     }
   
     break;
+  case "LEAVE":
   case "leave":
     window.alert("Leaving the store.");
 
